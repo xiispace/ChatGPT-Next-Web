7e1e84887c0be5275624ed5ea19555c1b5bfe291
xiispace <xiispace@163.com>
2024-05-14 03:00:33 +0000

    support gpt-4o

--- app/utils.ts
+++ app/utils.ts
@@ -297,6 +297,8 @@ export function isVisionModel(model: string) {
     "vision",
     "claude-3",
     "gemini-1.5-pro",
+    "gpt-4-turbo",
+    "gpt-4o",
   ];
 
   const isGpt4Turbo = model.includes("gpt-4-turbo") && !model.includes("preview");
